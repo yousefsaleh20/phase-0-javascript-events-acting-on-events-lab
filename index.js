f483abac2723c9a7d790713ca16fbe7fa2b42408
Yousaef Saleh <yousefsaleh20@icloud.com>
2022-08-01 16:11:38 -0500

    commit tree 9393d804fa918b531e385261db4e308bc5841981

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+/*const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.left = "0px";
+*/
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === 39){
+        moveDodgerRight();
+    }
+  })
